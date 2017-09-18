af6c0fd5df71fc4c55318ca233a82b0dda1f721c
Montrell Harrington <trellh@ufl.edu>
2017-09-18 17:53:58 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://Montrellh:mh143634@ds127034.mlab.com:27034/montrell', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
